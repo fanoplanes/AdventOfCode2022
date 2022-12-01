7d2f2a7e2046e0e10a142f2cd67664530aec25ac
FanoPlanes <pontryagin@protonmail.com>
2022-12-01 16:10:17 +0100

    uwu

--- 01/01-1.rkt
+++ 01/01-1.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-(define arr (file->string "input"))
+(define input-string (file->string "input"))
 
-(define arr-of-arr (map (lambda (a) (regexp-split #rx"\n" a)) (regexp-split #rx"\n\n" (regexp-replace #rx"\n$" arr ""))))
+(define list-of-list (map (lambda (a) (regexp-split #rx"\n" a)) (regexp-split #rx"\n\n" (regexp-replace #rx"\n$" input-string ""))))
 
-(define summed (map (lambda (a) (foldl + 0 a)) (map (lambda (a) (map (lambda (b) (string->number b)) a)) arr-of-arr)))
+(define summed (map (lambda (a) (foldl + 0 a)) (map (lambda (a) (map (lambda (b) (string->number b)) a)) list-of-list)))
 
 (display (car (sort summed >)))
--- 01/01-2.rkt
+++ 01/01-2.rkt
@@ -2,10 +2,10 @@
 
 (define sum-of-top 3)
 
-(define arr (file->string "input"))
+(define input-string (file->string "input"))
 
-(define arr-of-arr (map (lambda (a) (regexp-split #rx"\n" a)) (regexp-split #rx"\n\n" (regexp-replace #rx"\n$" arr ""))))
+(define list-of-list (map (lambda (a) (regexp-split #rx"\n" a)) (regexp-split #rx"\n\n" (regexp-replace #rx"\n$" input-string ""))))
 
-(define summed-sorted (sort (map (lambda (a) (foldl + 0 a)) (map (lambda (a) (map (lambda (b) (string->number b)) a)) arr-of-arr)) >))
+(define summed-sorted (sort (map (lambda (a) (foldl + 0 a)) (map (lambda (a) (map (lambda (b) (string->number b)) a)) list-of-list)) >))
 
 (display (foldl + 0 (take summed-sorted sum-of-top)))
