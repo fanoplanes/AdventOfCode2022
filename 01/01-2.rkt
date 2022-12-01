04dc278433b283975f835ec775464fe4e39654a3
FanoPlanes <pontryagin@protonmail.com>
2022-12-01 14:37:31 +0100

    uwu

--- 01/01-2.rkt
+++ 01/01-2.rkt
@@ -1,9 +1,11 @@
 #lang racket
 
+(define sum-of-top 3)
+
 (define arr (file->string "input"))
 
 (define arr-of-arr (map (lambda (a) (regexp-split #rx"\n" a)) (regexp-split #rx"\n\n" (regexp-replace #rx"\n$" arr ""))))
 
 (define summed-sorted (sort (map (lambda (a) (foldl + 0 a)) (map (lambda (a) (map (lambda (b) (string->number b)) a)) arr-of-arr)) >))
 
-(display (+ (first summed-sorted) (second summed-sorted) (third summed-sorted)))
+(display (foldl + 0 (take summed-sorted sum-of-top)))
