0c43d7eb04b3e61d0155950f72a2c7c01880cf30
FanoPlanes <pontryagin@protonmail.com>
2022-12-03 21:40:35 +0100

    uwu

--- 03/03-1.rkt
+++ 03/03-1.rkt
@@ -4,11 +4,15 @@
 
 (define split (string-split input-string))
 
-(define halved (map (lambda (str) (list (substring str 0 (/ (string-length str) 2)) (substring str (/ (string-length str) 2) (string-length str)))) split))
+(define halved (map (lambda (str) (list
+                                   (substring str 0 (/ (string-length str) 2))
+                                   (substring str (/ (string-length str) 2) (string-length str)))) split))
 
 (define halved-list (map (lambda (stri) (map (lambda (strng) (string->list strng)) stri)) halved))
 
-(define common (map (lambda (hlvd) (car (set-intersect (first hlvd) (second hlvd)))) halved-list))
+(define common (map (lambda (hlvd) (car (set-intersect
+                                         (first hlvd)
+                                         (second hlvd)))) halved-list))
 
 (define value (lambda (character) (cond
                                     [(char-upper-case? character) (- (char->integer character) 38)]
--- 03/03-2.rkt
+++ 03/03-2.rkt
@@ -4,7 +4,10 @@
 
 (define split-list (sequence->list (in-slice 3 input-list)))
 
-(define common (map (lambda (hlvd) (car (set-intersect (string->list (~a (first hlvd))) (string->list (~a (second hlvd))) (string->list (~a (third hlvd)))))) split-list))
+(define common (map (lambda (hlvd) (car (set-intersect
+                                         (string->list (~a (first hlvd)))
+                                         (string->list (~a (second hlvd)))
+                                         (string->list (~a (third hlvd)))))) split-list))
 
 (define value (lambda (character) (cond
                                     [(char-upper-case? character) (- (char->integer character) 38)]
