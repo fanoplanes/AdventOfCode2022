2171eadb9b27a6e3c117f998ad2d91b4c84bbe02
FanoPlanes <pontryagin@protonmail.com>
2022-12-03 16:34:38 +0100

    uwu

--- 03/03-2.rkt
+++ 03/03-2.rkt
@@ -2,12 +2,7 @@
 
 (define input-list (file->list "input2"))
 
-(define (split-by lst n)
-   (if (not (empty? lst))
-       (cons (take lst n) (split-by (drop lst n) n))
-       '() ))
-
-(define split-list (split-by input-list 3))
+(define split-list (sequence->list (in-slice 3 input-list)))
 
 (define common (map (lambda (hlvd) (car (set-intersect (string->list (~a (first hlvd))) (string->list (~a (second hlvd))) (string->list (~a (third hlvd)))))) split-list))
 
