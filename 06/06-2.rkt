a8edb560d8a11c40a947b7b6be1b061e59c8e301
FanoPlanes <pontryagin@protonmail.com>
2022-12-06 17:33:08 +0100

    unfucked day 06

--- 06/06-2.rkt
+++ 06/06-2.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (define pos 0)
-(define input (string->list (regexp-replace #rx"\n" (file->string "input") "")))
-(for/and ([i (in-range (- (length input) 13))]) (begin (set! pos (+ i 14))
-  (check-duplicates (list (list-ref input i) (list-ref input (+ i 1)) (list-ref input (+ i 2)) (list-ref input (+ i 3))(list-ref input (+ i 4))(list-ref input (+ i 5))(list-ref input (+ i 6))(list-ref input (+ i 7))(list-ref input (+ i 8))(list-ref input (+ i 9))(list-ref input (+ i 10))(list-ref input (+ i 11))(list-ref input (+ i 12))(list-ref input (+ i 13))))))
+(define input (regexp-replace #rx"\n" (file->string "input") ""))
+(for/and ([i (in-range (- (string-length input) 13))]) (begin (set! pos (+ i 14))
+  (check-duplicates (string->list (substring input i (+ i 14))))))
 (print pos)
